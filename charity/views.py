4108b70b80bfe16fb783a7023719270341408882
JakubG24 <jakub.gierczak24@gmail.com>
2021-06-15 20:46:36 +0200

    register, login

--- charity/views.py
+++ charity/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
 
 from charity.models import Donation, Institution
@@ -33,12 +37,39 @@ class IndexView(View):
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'login.html')
+        return render(request, 'registration/login.html')
+
+    def post(self, request):
+        username = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse_lazy('index'))
+        elif not User.objects.filter(username=username).exists():
+            return redirect(reverse_lazy('register'))
+        else:
+            return render(request, 'registration/login.html', {'msg': 'Błędne hasło'})
 
 
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        first_name = request.POST['name']
+        last_name = request.POST['surname']
+        username = request.POST['email']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            user = User(username=username, first_name=first_name, last_name=last_name, email=email)
+            user.password = make_password(password)
+            user.save()
+        else:
+            return render(request, 'register.html', {'msg': 'Podane hasła muszą być identyczne!'})
+        return redirect(reverse_lazy('login'))
 
 
 class AddDonationView(View):
