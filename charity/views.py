1b6603d68c698fe0cbc30577a5e0d38ba711e596
JakubG24 <jakub.gierczak24@gmail.com>
2021-06-15 12:47:44 +0200

    templates

--- charity/views.py
+++ charity/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
